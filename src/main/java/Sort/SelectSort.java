d37dd9faa13e921aa9cf4157431e7b583d6ac7c3
lin <linzhifuy@163.com>
2021-07-27 10:31:34 +0800

    third commit

--- src/main/java/Sort/SelectSort.java
+++ src/main/java/Sort/SelectSort.java
@@ -25,6 +25,17 @@ public class SelectSort {
         }
     }
 
+    //插入排序
+    public static void insertSort(int[] arr){
+        if(arr == null || arr.length < 2) return;
+
+        for (int i=1;i<arr.length;i++){
+            for(int j=i-1;j>=0&&arr[j]>arr[j+1];j--){
+                swap(arr,j,j+1);
+            }
+        }
+    }
+
     private static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
