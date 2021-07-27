b8b21e7d58e6c5471d535ffb89b3b44268285bc3
lin <linzhifuy@163.com>
2021-07-27 10:27:57 +0800

    third commit

--- src/main/java/Sort/SelectSort.java
+++ src/main/java/Sort/SelectSort.java
@@ -13,6 +13,18 @@ public class SelectSort {
         }
     }
 
+    public static void bubbleSort(int[] arr){
+        if(arr == null || arr.length < 2) return;
+
+        for(int e=arr.length-1;e>0;e--){
+            for(int i=0;i<e;i++){
+                if(arr[i]>arr[i+1]){
+                    swap(arr,i,i+1);
+                }
+            }
+        }
+    }
+
     private static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
